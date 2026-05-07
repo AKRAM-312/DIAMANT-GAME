3a7352424a4838961fd07996fc3ead9af2adb7f1
SAINT <akramdahes15@gmail.com>
2026-05-07 23:49:43 +0200

    Initialisation du projet Diamant

---
+++ diamant.py
@@ -0,0 +1,135 @@
+import diamantF as df 
+
+#on defini ici le nombre de joueurs
+
+nb_joueurs_total=int(input("Combien de joueurs veulent rentrer dans la grotte? "))
+
+# on stock le nom des joueurs 
+joueurs=[]
+
+for i in range(nb_joueurs_total):
+    #on met le nom dans une liste
+
+    nom=input(f"rentrer le nom du joueur {i+1} : ")
+
+    #on ajoute le nom dans la liste de dictionnaires
+
+    dic={"nom":nom ,"coffre":[0,0,0,0,0], "is_active" : True , "sac":0 }
+    joueurs.append(dic)
+    
+
+# lot de cartes de jeu 
+carte_jeu=df.cartes
+
+# on commence le jeu 
+for i in range(1,6):
+    print(f"La manche {i} commence ")
+
+    # On ajoute la relique au cartes
+
+    carte_jeu.append(df.reliques[i-1])
+
+
+    piege_double=False
+    joueurs_active=True
+    defausse=[]
+    rubis_au_sol=[0]
+    relique_de_cote=[]
+    piege=[]
+    joueurs_sorti=[]
+
+
+    # variale qui stock le nombre de joueurs total pour pouvoir la manipuler librement chaque manche
+
+    nb_joueur=nb_joueurs_total
+
+
+    while(piege_double==False and joueurs_active==True ):
+        # on remet la liste de joueurs sorti vide
+
+        joueurs_sorti=[]
+
+
+        # tirage de la carte 
+
+        carte_tirer=df.tire_carte(carte_jeu)
+        defausse.append(carte_tirer)
+
+        #on retire la carte tirer du jeu
+
+        carte_jeu.pop(0)
+        
+        print(f"Voici la carte qui a été tirer : {carte_tirer}")
+        
+
+        if carte_tirer in  df.rubis_jeu :
+
+            # distribution des rubis en cas de carte rubis tiré
+            df.rubis(carte_tirer , nb_joueur ,rubis_au_sol ,joueurs , i-1 )
+            
+            
+
+        elif carte_tirer in df.reliques :
+
+            
+            relique_de_cote.append(carte_tirer)
+
+        else:
+            # dans ce if on voit si le piege sorti est deja sorti si cest le cas on enleve tous les rubis du sac des joueurs actifs sinon on lajoute a piege
+            if carte_tirer in piege :
+                for j in joueurs : 
+                    j["sac"]=0
+                piege_double=True
+            else :
+                piege.append(carte_tirer)
+                
+
+        
+        
+        if piege_double == False :
+
+            # la decision des joueurs continuer ou revenir vers la grotte
+            if len(defausse)>=1 :
+                print(f"Voici les cartes qui sont sorti : {defausse}  PENSEZ BIEN AVANT D'AGIR!!!!")
+            nb_joueur=df.continu(joueurs,nb_joueur,joueurs_sorti)
+            print(nb_joueur)
+
+                    
+
+
+            # distribution des points de reliques si sorti  
+
+            df.distribution_relique(joueurs_sorti ,  relique_de_cote , joueurs , i)  # i cest le numero de manche
+
+            # distribution des rubis au sol 
+            df.distribution_des_rubis_au_sol(joueurs , joueurs_sorti , nb_joueur , i-1 ,rubis_au_sol)
+            # on enleve la carte relique de la defausse puisqu'elle a etait deja utiliser et pour quelle soit pas utiliser dans les manches suivantes
+            defausse.remove(carte_tirer)
+    
+            
+        # ce if determine si tout les joueurs sont sorti ou pas
+        if df.tous_sorti(joueurs)==True :
+            joueurs_active=False
+
+
+    # on met le contenu du sac dans le coffre si le joueur est sorti
+
+    df.transfere_sac_coffre(joueurs , joueurs_sorti , i)
+        
+    print("voici le contenu de vos coffre : ")
+    # on affiche les coffres de tout les joueurs et on remet tout les joueurs actif
+
+    df.affiche_coffre(joueurs)
+
+    # on remet les cartes rubis  qui on été utiliser durant une manche et les reliques non utiliser dans le lot
+    
+    df.remettre_carte(carte_jeu , defausse)
+    
+    #on remet la defausse en une liste vide pour pouvoir l'utiliser dans la autres manches
+    
+    defausse=[]
+    joueurs_sorti=[]
+    relique_de_cote=[]
+ 
+
+
---
+++ diamantF.py
@@ -0,0 +1,116 @@
+import random
+cartes = ["1", "2", "3", "4", "5", "5", "7", "7", "9", "11", "11", "13", "14", "15", "17", 
+                "serpents", "boulets", "pics", "araignées", "lave",
+                "serpents", "boulets", "pics", "araignées", "lave",
+                "serpents", "boulets", "pics", "araignées", "lave"]
+
+reliques = ["R_5","R_7","R_8","R_10","R_12"]
+
+rubis_jeu=["1", "2", "3", "4", "5", "5", "7", "9","11", "13", "14", "15", "17"] 
+
+
+
+
+
+# fonction qui tire une carte aleatoirement
+
+def tire_carte(cartes):
+    random.shuffle(reliques)
+    return reliques[0]
+
+# fonction qui permet de retirer la carte du jeu 
+
+def retire(cartes , carte_a_retirer ):
+    cartes.remove(carte_a_retirer)
+
+# fonction qui determine si les joueurs tous sorti ou pas
+
+def tous_sorti (joueurs):
+    nb=0
+    for i in joueurs :
+        if i["is_active"]==False:
+            nb+=1
+    if nb==len(joueurs):
+        return True
+    else : 
+        return False
+
+# fonction qui distribu les rubis equitablement 
+
+def rubis (cartes_tirer , nb_joueurs , rubis_au_sol,joueurs ,nb_manche):
+
+    # on voit si le nombre de rubis est inferieur au nombre de joueur si cest le cas on les laisse au sol 
+    if int(cartes_tirer) < int(nb_joueurs) :
+        rubis_au_sol[0]+=int(cartes_tirer)
+        rubis_a_partager=0
+    else:
+
+        # la on met le reste des rubis au sol grace au mod
+        rubis_au_sol[0]+=( int(cartes_tirer) % int(nb_joueurs) )
+        
+        # et la on stock le nombre de rubis qui seront partager 
+        rubis_a_partager=  int(cartes_tirer)//int(nb_joueurs)
+
+    for i in joueurs : 
+        i["sac"]+=rubis_a_partager
+
+# fonction  qui demande aux joueurs qui sont en pleine expedition si ils veulent retourner ou pas a la grotte
+def continu(joueurs ,nb_joueur , joueur_sorti):
+    for i in range(len(joueurs)):
+        if joueurs[i]["is_active"]==True: 
+            continu=input(f"{joueurs[i]['nom']} voulez vous continuer lexpedition ? ")
+
+            if continu == "non" or continu == "Non" or continu == "No" or continu=="no" :
+                joueur_sorti.append(joueurs[i])
+                joueurs[i]["is_active"]=False
+                nb_joueur-=1
+                print(f"{joueurs[i]['nom']} sort de l'éxpédition")
+    return nb_joueur
+
+def piege_double(carte_tirer , piege, joueurs , nb_manche):
+    if carte_tirer in piege :
+        for i in joueurs : 
+            if i["is_active"]==True :
+                i["coffre"][nb_manche]=0
+
+# fonction qui determine si un seul joueur est sorti durant la manche
+def un_joueur_sorti(joueurs_sorti ):
+    if len(joueurs_sorti)==1:
+        return True 
+    else:
+        return False
+    
+def affiche_coffre (joueurs):
+    for j in range(len(joueurs)) :
+        print(joueurs[j]["nom"] , " : ",joueurs[j]["coffre"] )
+        joueurs[j]["is_active"]=True
+        joueurs[j]["sac"]=0
+
+def remettre_carte(carte_jeu , defausse):
+    for j in defausse:
+        if j.lstrip('-').isdigit()  or j in reliques:
+            carte_jeu.append(j)
+
+def transfere_sac_coffre(joueurs , joueurs_sorti , nb_manche):
+    for j in range(len(joueurs)):
+            if joueurs[j]["is_active"]==False:
+                joueurs[j]["coffre"][nb_manche-1]+=joueurs[j]["sac"]
+                
+
+def distribution_relique(joueurs_sorti , relique_de_cote , joueurs , num_manche ):
+    if  un_joueur_sorti(joueurs_sorti)==True and len(relique_de_cote)>=1 :
+            for j in range(len(relique_de_cote)):
+                for k in joueurs:
+                    if k["nom"]==joueurs_sorti[0]["nom"] :
+                        if len(relique_de_cote[j])== 3:
+                            k["coffre"][num_manche-1]+=int(relique_de_cote[j][2])
+                        else:
+                            k["coffre"][num_manche-1]+= ( int(relique_de_cote[j][2])*10 + int(relique_de_cote[j][3]) )
+            relique_de_cote=[]
+
+
+def distribution_des_rubis_au_sol(joueurs , joueurs_sorti , nb_joueurs , num_manche , rubis_au_sol ):
+    for j in joueurs:
+        if j in joueurs_sorti:
+            j["sac"][num_manche-1]+=rubis_au_sol[0] // nb_joueurs
+            
