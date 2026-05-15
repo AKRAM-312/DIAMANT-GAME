515fa95146a9adf98125a2b2cb89846de73f2c91
AKRAM-312 <akramdahes15@gmail.com>
2026-05-15 22:40:18 +0200

    Ajout de ma strategie en utilisant les fonctions ajouter

--- strategie.py
+++ strategie.py
@@ -41,3 +41,43 @@ class strategie_test1:
             return False
         else: 
             return True
+
+
+class strat_abs :
+    def __init__(self):
+        pass
+    def play(self,
+        mon_coffre, # liste d'entiers de taille nb_manches
+        mon_sac, # entier : nombre de rubis
+        rubis_au_sol, # rubis restants à partager
+        id_manche, # entier : compris entre 1 et 5
+        les_joueurs,    # [ {"coffre":[2,5,0,0,0], "is_active" : True}
+                             #,... ,
+                             # {"coffre": [0,15,3,0,0], "is_active" : False} ]
+        tas_tri, # le tas de cartes restantes (pas dans l'ordre de tirage)
+        defausse, # ce qui est déjà joué comme cartes
+        relique_de_cote,
+        nb_joueurs_actif
+            ):
+        if df.prise_risque(les_joueurs , mon_coffre , id_manche)==True:
+            if df.proba_mort <0.3:
+            
+                nb_joueurs_sort=df.estimation_sorti_joueurs(les_joueurs ,defausse ,id_manche , rubis_au_sol , relique_de_cote ,nb_joueurs_actif)
+            
+                if df.calcul_gain_possible(mon_sac , rubis_au_sol , relique_de_cote , nb_joueurs_sort) >= 25 :
+                    return False
+                else: 
+                    return True
+            else : 
+                return False
+        else:
+            if df.proba_mort(defausse , tas_tri) <0.6:
+            
+                nb_joueurs_sort=df.estimation_sorti_joueurs(les_joueurs ,defausse ,id_manche , rubis_au_sol , relique_de_cote ,nb_joueurs_actif)
+            
+                if df.calcul_gain_possible(mon_sac , rubis_au_sol , relique_de_cote , nb_joueurs_sort) >= 17 :
+                    return False
+                else: 
+                    return True
+            else : 
+                return False
